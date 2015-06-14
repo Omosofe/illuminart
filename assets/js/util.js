3306d20435ebef86282ceba3846ffe4228f1c793
Sudharsanan <sudhar.ti@gmail.com>
2015-06-13 19:52:16 -0500

    Add sticky back to top button

--- assets/js/util.js
+++ assets/js/util.js
@@ -9,16 +9,26 @@ $(document).ready(function(){
     shuffle: true,
     transition: ['fade', 'zoomOut', 'fade2'],
     slides: [
-        { src: 'assets/img/background-2.jpg' },
-        { src: 'assets/img/background-3.jpg' },
-        { src: 'assets/img/background-4.jpg' },
-        { src: 'assets/img/background.jpg' }
+    { src: 'assets/img/background-2.jpg' },
+    { src: 'assets/img/background-3.jpg' },
+    { src: 'assets/img/background-4.jpg' },
+    { src: 'assets/img/background.jpg' }
     ]
   });
 
   $("a[href*=#]").click(function(e){
     e.preventDefault();
     scrollDiv($(this).attr("href"));
+  });
+
+  var offset = 250;
+  var duration = 300;
+  jQuery(window).scroll(function() {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery('.back-to-top').fadeIn(duration);
+    } else {
+      jQuery('.back-to-top').fadeOut(duration);
+    }
   });
 
 });
