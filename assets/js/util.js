0814d359f9c50b94eeb04ca6cee4b7911420fcd3
Sudharsanan <sudhar.ti@gmail.com>
2015-06-13 19:12:01 -0500

    scroll to div from link, add contact info

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,3 +1,7 @@
+function scrollDiv(element) {
+  $("html, body").delay(250).animate({ scrollTop: $(element).offset().top }, 750);
+};
+
 $(document).ready(function(){
   $('body').vegas({
     delay: 3500,
@@ -11,4 +15,10 @@ $(document).ready(function(){
         { src: 'assets/img/background.jpg' }
     ]
   });
+
+  $("a[href*=#]").click(function(e){
+    e.preventDefault();
+    scrollDiv($(this).attr("href"));
+  });
+
 });
